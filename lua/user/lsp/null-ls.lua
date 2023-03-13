469b38f4f8d60f1f9a1baf6f1b9b8f2cc08623c6
Ruofan Wong <meimprankster@gmail.com>
2023-03-13 14:12:40 +0800

    Add hop

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,9 +8,12 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup {
   debug = false,
+  autostart = true,
   sources = {
     formatting.prettier.with {
       command = "prettier",
@@ -22,4 +25,17 @@ null_ls.setup {
     formatting.google_java_format,
     diagnostics.flake8,
   },
+  on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.formatting_sync()
+                end,
+            })
+        end
+    end,
 }
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -72,6 +72,14 @@ return packer.startup(function(use)
     end,
   }
   use "karb94/neoscroll.nvim"
+  use {
+    "phaazon/hop.nvim",
+    branch = "v2", -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require("hop").setup { keys = "etovxqpdygfblzhckisuran" }
+    end,
+  }
 
   -- Colorschemes
   use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
